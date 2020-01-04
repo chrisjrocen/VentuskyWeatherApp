1bf432b619fae4fe28a4883a3292691152988ef6
chrisjrocen <43829976+chrisjrocen@users.noreply.github.com>
2020-01-04 16:36:19 +0300

    Webview app with navigation drawer and splash screen activities
    A webview application that connects to  ventusky.com
    It is a navigation drawer with a preliminary splash screen

---
+++ app/src/main/java/com/example/weatherventusky/ui/about/AboutFragment.kt
@@ -0,0 +1,56 @@
+package com.example.weatherventusky.ui.about
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.example.weatherventusky.R
+
+
+class AboutFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        val v = inflater.inflate(R.layout.fragment_about, container, false)
+        val mWebView = v.findViewById<View>(R.id.webviewAbout) as WebView
+        mWebView.loadUrl("https://www.ventusky.com/about")
+
+        // Enable Javascript
+        val webSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.webViewClient = WebViewClient()
+
+        mWebView.canGoBack()
+        mWebView.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK
+                    && event.action == MotionEvent.ACTION_UP
+                    && mWebView.canGoBack()) {
+                mWebView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
+
+
+
+        return v
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //you can set the title for your toolbar here for different fragments different titles
+        activity!!.title = "Home"
+    }
+
+
+}
---
+++ app/src/main/java/com/example/weatherventusky/ui/home/HomeFragment.kt
@@ -0,0 +1,57 @@
+package com.example.weatherventusky.ui.home
+
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.example.weatherventusky.R
+
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
+        val mWebView = v.findViewById<View>(R.id.webViewHome) as WebView
+        mWebView.loadUrl("https://www.ventusky.com")
+
+        // Enable Javascript
+        val webSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.webViewClient = WebViewClient()
+
+        mWebView.canGoBack()
+        mWebView.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.action == MotionEvent.ACTION_UP
+                && mWebView.canGoBack()) {
+                mWebView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
+
+
+
+        return v
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //you can set the title for your toolbar here for different fragments different titles
+        activity!!.title = "Home"
+    }
+
+
+}
